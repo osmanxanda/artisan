557756f1ad2abe0b775c1cc2d3d480face53bff2
osmanxanda <127098638+osmanxanda@users.noreply.github.com>
2023-09-07 13:18:15 +0100

    cli

--- src/main.go
+++ src/main.go
@@ -2,21 +2,22 @@ package main
 
 import (
 	"fmt"
+	"github.com/urfave/cli"
 	"log"
 	"os"
 )
 
 func main() {
-	app := &cli.App{
-		Name:  "boom",
-		Usage: "make an explosive entrance",
-		Action: func(*cli.Context) error {
-			fmt.Println("boom! I say!")
-			return nil
-		},
+	app := cli.NewApp()
+	app.Name = "boom"
+	app.Usage = "make an explosive entrance"
+	app.Action = func(c *cli.Context) error {
+		fmt.Println("boom! I say!")
+		return nil
 	}
 
-	if err := app.Run(os.Args); err != nil {
+	err := app.Run(os.Args)
+	if err != nil {
 		log.Fatal(err)
 	}
 }
