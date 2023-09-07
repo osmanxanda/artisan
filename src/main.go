243351ef6dbead9fbd1b14dc6d909cf1f938198c
osmanxanda <127098638+osmanxanda@users.noreply.github.com>
2023-09-07 13:12:00 +0100

    test

---
+++ src/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	app := &cli.App{
+		Name:  "boom",
+		Usage: "make an explosive entrance",
+		Action: func(*cli.Context) error {
+			fmt.Println("boom! I say!")
+			return nil
+		},
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
+}
